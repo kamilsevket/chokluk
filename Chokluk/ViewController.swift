cf3c8f7b7898deb4b9bc30423e252297ebb6c803
Kamil Şevket Demirtaş <kamilsevket@hotmail.com>
2017-10-06 21:16:25 +0300

    Initial Commit

---
+++ Chokluk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chokluk
+//
+//  Created by Kamil on 6.10.2017.
+//  Copyright © 2017 Kamil Demirtaş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
